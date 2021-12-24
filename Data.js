0c4931abf1887087f2f1407fbd52950e9c000639
AhmadiZul <90020374+AhmadiZul@users.noreply.github.com>
2021-12-24 21:07:41 +0700

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,38 @@
+export const Data = [
+  {
+    id: 1,
+    image: "motul51004t10w40.jpg",
+    name: "Spare Part Oli Motor Motul 5100 4t 10w40",
+    price: 129,
+  },
+  {
+    id: 2,
+    image: "Ban_Michelin_Pilot_Road_5_160_60_Ring_17_Ban_Motor_Ninja.jpg",
+    name: "Spare Part Ban Luar Motor 160/60-17 R MICHELIN",
+    price: 290,
+  },
+  {
+    id: 3,
+    image: "ngk_ngk_busi_iridium_for_suzuki_satria_r_-_suzuki_rk_cool-_yamaha_rxz_bpr8eix_full02_e0k9mb3h.jpg",
+    name: "Spare Part Busi Motor NGK Iridium BPR8EIX",
+    price: 88,
+  },
+  {
+    id: 4,
+    image: "kampas rem.jpg",
+    name: "Spare Part Kampas Rem Daytona Superstock",
+    price: 65,
+  },
+  {
+    id: 5,
+    image: "lampu led.jpg",
+    name: "Spare Part Lampu Depan Led Philips M5",
+    price: 65,
+  },
+  {
+    id: 6,
+    image: "velg motor.jpg",
+    name: "Spare Part Velg Motor RCB Racing Boy",
+    price: 2500,
+  }
+];
